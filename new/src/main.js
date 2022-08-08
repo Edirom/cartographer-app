6e914139c10d298f6468fa09a87003f8a5bfe687
Hizkiel Alemayehu <44091398+Hizkie@users.noreply.github.com>
2022-08-08 16:37:00 +0200

    Delete new directory

--- new/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-
-import 'spectre.css/dist/spectre-exp.css'
-import 'spectre.css/dist/spectre-icons.css'
-import 'spectre.css/dist/spectre.css'
-
-createApp(App).use(store).mount('#app')
