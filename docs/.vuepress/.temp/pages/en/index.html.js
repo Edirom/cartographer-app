24219b4be025811923c533500569ee82dc87c15a
Hizkiel <hizclick@gmail.com>
2025-08-28 18:06:45 +0200

    Cleaning up for deployment

---
+++ docs/.vuepress/.temp/pages/en/index.html.js
@@ -0,0 +1,16 @@
+import comp from "/usr/app/docs/.vuepress/.temp/pages/en/index.html.vue"
+const data = JSON.parse("{\"path\":\"/en/\",\"title\":\"Cartographer-app Dokumentation\",\"lang\":\"en-US\",\"frontmatter\":{\"home\":true,\"title\":\"Cartographer-app Dokumentation\",\"heroText\":\"Cartographer-app\",\"tagline\":\"Dokumentation für die Cartographer App\",\"actions\":[{\"text\":\"Loslegen\",\"link\":\"/de/user/\",\"type\":\"primary\"},{\"text\":\"Technische Doku\",\"link\":\"/de/technical/README.html\",\"type\":\"secondary\"}],\"features\":[{\"title\":\"Benutzerhandbuch\",\"details\":\"So benutzt du die App.\"},{\"title\":\"Entwickler-Doku\",\"details\":\"Komponenten, Store, Tools.\"}]},\"git\":{},\"filePathRelative\":\"en/README.md\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
